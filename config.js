611cb4c6e7f2150670b09405d1dc88828a449a31
Cyberattack11 <157394843+Cyberattack11@users.noreply.github.com>
2024-01-24 00:30:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -13,14 +13,14 @@ global.owner = [
   ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
   ['233533763772', 'Abraham Dwamena2', true],
   ['233531910067', 'Abraham Dwamena3', true],
-  
+  ['2347025860706', 'Attack Sama', true]
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['923184070915', '94789481495', '923184474176','233531910067','233208179431'] 
-global.prems = ['923184070915','233208179431','94789481495', '918360234087','233531910067']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087','233531910067']
+global.mods = ['923184070915', '94789481495', '923184474176','233531910067','233208179431','2347025860706']
+global.prems = ['923184070915','233208179431','94789481495', '918360234087','233531910067','2347025860706']
+global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087','233531910067','2347025860706']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -57,7 +57,7 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = 'XLICON-V2'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
+global.packname = 'ATTACK-BOT'
 global.author = '@XLICON-V2' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
